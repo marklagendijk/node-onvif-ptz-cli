52f0a6af26d3d8bb6bbc67818481dd8a3474c6da
Mark Lagendijk <mark@lagendijk.nl>
2020-07-01 15:29:04 +0200

    Further CLI implementation

--- bin/cli.js
+++ bin/cli.js
@@ -30,9 +30,40 @@ const argv = require('yargs')
             type: 'string'
         }
     })
-    .command('goto-preset', 'Load a preset.', {
+    .command('goto-preset', 'Move the camera to a preset.', {
         preset: {
             describe: 'The preset which should be loaded.',
+            demandOption: true,
+            type: 'string'
+        }
+    })
+    .command('goto-home', 'Move the camera to the home position.')
+    .command('move', 'Move relatively to the current camera position.', {
+        x: {
+            describe: 'Movement on the x-axis. Left: between -1 and 0. Right: between 0 and 1. Optionally add \':NUMBER\' for speed (between 0 and 1).',
+            type: 'string',
+            default: '0'
+        },
+        y: {
+            describe: 'Movement on the y-axis. Down: between -1 and 0. Up: between 0 and 1. Optionally add \':NUMBER\' for speed (between 0 and 1).',
+            type: 'string',
+            default: '0'
+        },
+        z: {
+            describe: 'Movement on the z-axis (zoom). Zoom out: between -1 and 0. Zoom in: between 0 and 1. Optionally add \':NUMBER\' for speed (between 0 and 1).',
+            type: 'string',
+            default: '0'
+        }
+    })
+    .command('get-presets', 'List all camera presets. Note: for some cameras the ONVIF presets are separate from the normal presets. If that is the case, you may have to re-create your presets using the set-preset command.')
+    .command('set-preset', 'Store the current location/settings in a preset.', {
+        preset: {
+            describe: 'The token of the preset. Usually just a number.',
+            demandOption: true,
+            type: 'string'
+        },
+        name: {
+            describe: 'The name of the preset.',
             demandOption: true,
             type: 'string'
         }
@@ -44,6 +75,30 @@ const argv = require('yargs')
     .example(
         'onvif-ptz goto-preset --baseUrl=http://192.168.0.123:8080 -u=admin -p=admin --preset=1',
         'Load preset "1" on a camera that uses port 8080 as onvif port.'
+    )
+    .example(
+        'onvif-ptz goto-home --baseUrl=http://192.168.0.123 -u=admin -p=admin',
+        'Move the camera to the home position.'
+    )
+    .example(
+        'onvif-ptz move --baseUrl=http://192.168.0.123:8080 -u=admin -p=admin -x=0.01',
+        'Move the camera 0.01 to the right.'
+    )
+    .example(
+        'onvif-ptz move --baseUrl=http://192.168.0.123:8080 -u=admin -p=admin -x=0.01:0.5',
+        'Move the camera 0.01 to the right with half speed.'
+    )
+    .example(
+        'onvif-ptz move --baseUrl=http://192.168.0.123:8080 -u=admin -p=admin -x=0.01 -y=0.02 -z=0.03',
+        'Move the camera 0.01 to the right, 0.02 to the top and zoom in 0.03.'
+    )
+    .example(
+        'onvif-ptz get-presets --baseUrl=http://192.168.0.123 -u=admin -p=admin',
+        'List all camera presets.'
+    )
+    .example(
+        'onvif-ptz set-preset --baseUrl=http://192.168.0.123 -u=admin -p=admin --preset=1 --name=Overview',
+        'Create a preset with preset token 1 and name Overview.'
     )
     .demandCommand()
     .env('ONVIF')
@@ -69,14 +124,21 @@ const argv = require('yargs')
             case 'goto-home':
                 await ptz.gotoHomePosition(device);
                 break;
-            case 'relative-move':
-                ptz.relativeMove(device, {
-                    movementX: argv.movementX,
-                    movementY: argv.movementY,
-                    movementZ: argv.movementZ,
-                    speedX: argv.speedX,
-                    speedY: argv.speedY,
-                    speed: argv.speedZ,
+            case 'move':
+                const x = argv.x.split(':');
+                const y = argv.y.split(':');
+                const z = argv.z.split(':');
+                await ptz.relativeMove(device, {
+                    movement: {
+                        x: x[0],
+                        y: y[0],
+                        z: z[0],
+                    },
+                    speed: {
+                        x: x[1] || 1,
+                        y: y[1] || 1,
+                        z: z[1] || 1
+                    }
                 });
                 break;
             case 'get-presets':
